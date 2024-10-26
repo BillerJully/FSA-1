6d850a22f06d9fbbc61b192f9eb19f93958082e8
billerjully <billerjully@gmail.com>
2024-10-26 15:44:00 +0300

    try 1

--- client/src/components/transactionGeneralizedData/TransactionGeneralizedData.js
+++ client/src/components/transactionGeneralizedData/TransactionGeneralizedData.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import styles from './TransactionGeneralizedData.module.css'
 
-export default function TransactionGeneralizedData({ title }) {
+export default function TransactionGeneralizedData({
+    title,
+    transactionsByPeriod,
+}) {
+    console.log(transactionsByPeriod)
     return (
         <div className={styles.transactionGeneralizedContainer}>
             <h3>{title}</h3>
