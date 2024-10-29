e9abddf573a42114ccc0720f83ebf3e77e91678f
billerjully <billerjully@gmail.com>
2024-10-29 15:54:05 +0300

    added env

---
+++ server/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+UserSchema.methods.isValidPassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+export default mongoose.model('User ', UserSchema)
